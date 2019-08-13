e332b5550268c4abaf3f329bc126b6062201771c
agronbach <alex.gronbach+github@gmail.com>
2019-08-13 10:00:14 -0400

    Delete includes.js
    Moving to /includes/ to better match site structure

--- includes.js
+++
@@ -1,7 +0,0 @@
-// Allow HTML "includes" of header/footer/favIcon code
-$(function(){
-    $("[data-load]").each(function(){
-        $(this).load($(this).data("load"), function(){
-        });
-    });
-})
